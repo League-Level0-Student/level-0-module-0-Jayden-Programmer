bbee82c435686c9e782d2e82772b93ccf65d79d0
root <root@a6b49e41d733>
2020-03-24 02:50:56 +0000

    Code_5

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,21 +12,46 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
+    	Robot Rob = new Robot(); 
 
 
         // 3. Put the robot's pen down
+    	Rob.penDown(); 
 
 
         // 6. Make the robot move as fast as possible
+    	
+    	Rob.setSpeed(100);
+    	
 
 
         // 5. Do everything below here 4 times
+for (int i = 0; i < 4; i++) {
+	Rob.move(200);	
+	Rob.turn(90);
 
 
+
+
+
+
+
+
+
+
+
+
+}
+    	
         //         2. Move your robot 200 pixels
+    	
+    	
+    	
+    	
 
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
+    	
 
 
     }
---
+++ src/_02_robot_graffiti/RobotGrafiti.java
@@ -0,0 +1,17 @@
+package _02_robot_graffiti;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGrafiti {
+	public static void main(String[] args) {
+		
+		Robot Rob = new Robot();
+		Rob.penDown(); 
+		Rob.setSpeed(100);
+	}
+
+	
+	
+
+ 
+
